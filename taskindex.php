5cd4ec3edcca84f28d7f9cdc13e965d370ad59f6
Anurag Kumar <anurag991012@gmail.com>
2020-06-18 15:09:55 +0530

    Update taskindex.php

--- taskindex.php
+++ taskindex.php
@@ -1,5 +1,5 @@
 <body bgcolor='aqua'>
-<img src='' height='1920' width='1200' />
+<img src='https://anurag07hawkbucket1.s3.ap-south-1.amazonaws.com/Wallpaper2_1920x1200.jpg' height='1920' width='1200' />
 <pre>
 <?php
 print "Mclaren F1 Formula One Team"
